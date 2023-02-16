115a6cf84cbbf05005bb1f69711112e60e0581b5
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2023-02-16 22:39:22 +0100

    exo

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// find the sum of first n natural numbers using recursion
+namespace Program;
+
+class SumOfNaturalTask{
+
+static int sum = 0;
+public static int SumOfNatural(int num){
+
+
+if (num<1)
+{
+    return sum;
+}
+
+sum += num;
+num--;
+return SumOfNatural(num);
+
+}
+
+public static void Main (string[] args){
+   Console.Write (SumOfNatural(4));
+}
+
+}
